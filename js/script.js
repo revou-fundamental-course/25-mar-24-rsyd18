63b64ac2772a1d1ffa31b355ac892c1a5441fd3d
Rasyid Dwi Kurniawan <rasyiddwi05@gmail.com>
2024-04-04 23:57:40 +0700

    final commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function calculate() {
+    let weightInput = document.getElementById('weight').value;
+    console.log(weightInput.value);
+
+    if (weightInput == '') {
+        
+        console.log('Kosong')
+    } else {
+        console.log('Ada isi')
+    }
+}
